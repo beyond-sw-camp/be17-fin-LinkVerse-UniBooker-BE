f78fb6aed7939ba831fca3937833ae9d0ceb4ff5
seoyoen22 <hsy2709@gmail.com>
2025-10-02 14:47:08 +0900

    [chore] 기본 프로젝트 생성

---
+++ src/main/java/org/example/unibooker/UnibookerApplication.java
@@ -0,0 +1,13 @@
+package org.example.unibooker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UnibookerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UnibookerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/unibooker/UnibookerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.unibooker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UnibookerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
