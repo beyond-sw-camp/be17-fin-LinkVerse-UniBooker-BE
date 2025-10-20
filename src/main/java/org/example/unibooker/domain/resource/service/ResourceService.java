fa35f6db95b7d4aedfd2ee46e155a511d1b2dc1e
thay123028 <thay123028@naver.com>
2025-10-20 10:46:50 +0900

    [Feat] 리소스 삭제, 리소스 활성화/비활성화 상태 변경 로직 구현

--- src/main/java/org/example/unibooker/domain/resource/service/ResourceService.java
+++ src/main/java/org/example/unibooker/domain/resource/service/ResourceService.java
@@ -1,6 +1,8 @@
 package org.example.unibooker.domain.resource.service;
 
+import jakarta.persistence.OptimisticLockException;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.example.unibooker.domain.resource.model.ResourceDto;
 import org.example.unibooker.domain.resource.model.ResourceGroups;
 import org.example.unibooker.domain.resource.model.ResourceStatus;
@@ -16,6 +18,7 @@ import java.time.LocalTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class ResourceService {
@@ -80,5 +83,75 @@ public class ResourceService {
                 .orElseThrow(() -> new IllegalArgumentException("해당 리소스가 존재하지 않습니다."));
 
         resource.update(dto);
+    }
+
+
+    // -------------------- 리소스 삭제 --------------------
+    @Transactional
+    public void deleteResource(Long resourceId) {
+        try {
+            Resources resource = resourceRepository.findByIdAndIsActiveTrueAndDeletedAtIsNull(resourceId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 리소스가 존재하지 않습니다."));
+
+            if (!Boolean.TRUE.equals(resource.getIsActive())) {
+                throw new IllegalArgumentException("이미 비활성화 또는 삭제된 리소스입니다.");
+            }
+
+            resource.setIsActive(false);
+            resource.softDelete();
+
+        } catch (OptimisticLockException e) {
+            throw new IllegalStateException("다른 사용자가 동시에 수정 중입니다. 잠시 후 다시 시도해주세요.", e);
+        }
+    }
+
+
+    // -------------------- 리소스 활성화 --------------------
+    @Transactional
+    public void activate(Long resourceId) {
+
+        try {
+            // TODO : 플랫폼 관리자 권한을 가졌는지 확인
+
+            Resources resource = resourceRepository.findByIdAndDeletedAtIsNull(resourceId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 리소스 그룹이 존재하지 않습니다."));
+
+            if (Boolean.TRUE.equals(resource.getIsActive())) {
+                log.info("이미 활성화된 서비스입니다. id={}", resourceId);
+                return;
+            }
+
+            resource.setIsActive(true);
+            // resourceGroup.setUpdatedBy(user); // 수정자 추후 추가
+
+            log.info("서비스 활성화 완료 - id={}", resourceId);
+        } catch (OptimisticLockException e) {
+            throw new IllegalStateException("다른 사용자가 동시에 수정 중입니다. 다시 시도해주세요.");
+        }
+    }
+
+
+    // -------------------- 리소스 비활성화 --------------------
+    @Transactional
+    public void deactivate(Long resourceId) {
+
+        try {
+            // TODO : 플랫폼 관리자 권한을 가졌는지 확인
+
+            Resources resource = resourceRepository.findByIdAndDeletedAtIsNull(resourceId)
+                    .orElseThrow(() -> new IllegalArgumentException("해당 리소스 그룹이 존재하지 않습니다."));
+
+            if (Boolean.FALSE.equals(resource.getIsActive())) {
+                log.info("이미 활성화된 서비스입니다. id={}", resourceId);
+                return;
+            }
+
+            resource.setIsActive(false);
+            // resourceGroup.setUpdatedBy(user); // 수정자 추후 추가
+
+            log.info("서비스 비활성화 완료 - id={}", resourceId);
+        } catch (OptimisticLockException e) {
+            throw new IllegalStateException("다른 사용자가 동시에 수정 중입니다. 다시 시도해주세요.");
+        }
     }
 }
