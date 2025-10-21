2e83a07900ef57ef96be7547693627be5c4300fb
thay123028 <thay123028@naver.com>
2025-10-21 15:41:31 +0900

    [Feat] 카테고리 필드 조회할 때 삭제된 거 제외하고 조회도록 수정

--- src/main/java/org/example/unibooker/domain/resource/repository/CategoryFieldDefinitionRepository.java
+++ src/main/java/org/example/unibooker/domain/resource/repository/CategoryFieldDefinitionRepository.java
@@ -6,8 +6,17 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface CategoryFieldDefinitionRepository extends JpaRepository<CategoryFieldDefinitions, Long> {
-    List<CategoryFieldDefinitions> findByCategory(ServiceCategory category);
+
+    // 카테고리별 삭제되지 않은 필드 조회
+    List<CategoryFieldDefinitions> findByCategoryAndDeletedAtIsNull(ServiceCategory category);
+
+    // 삭제되지 않은 전체 필드 조회
+    List<CategoryFieldDefinitions> findByDeletedAtIsNull();
+
+    // 삭제되지 않은 단일 필드 조회
+    Optional<CategoryFieldDefinitions> findByIdAndDeletedAtIsNull(Long id);
 }
