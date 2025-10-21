e8fd75eb3b02e0f4b15c5e8f87d508616e41d26c
thay123028 <thay123028@naver.com>
2025-10-21 15:17:10 +0900

    [Feat] 카테고리 필드 조회 DTO에 fromEntity 추가

--- src/main/java/org/example/unibooker/domain/resource/model/CategoryFieldDto.java
+++ src/main/java/org/example/unibooker/domain/resource/model/CategoryFieldDto.java
@@ -5,6 +5,7 @@ import lombok.Builder;
 import lombok.Getter;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Schema(description = "카테고리 별 필수 필드 관련 DTO 클래스들")
 public class CategoryFieldDto {
@@ -53,6 +54,15 @@ public class CategoryFieldDto {
 
         @Schema(description = "필드 적용할 카테고리", example = "RESERVATION")
         private ServiceCategory category;
+
+        public static CategoryFieldDetailRes fromEntity(CategoryFieldDefinitions entity) {
+            return CategoryFieldDetailRes.builder()
+                    .fieldName(entity.getFieldName())
+                    .description(entity.getDescription())
+                    .dataType(entity.getDataType())
+                    .category(entity.getCategory())
+                    .build();
+        }
     }
 
 
@@ -63,6 +73,15 @@ public class CategoryFieldDto {
 
         @Schema(description = "카테고리 필드 목록")
         private List<CategoryFieldDetailRes> categoryFields;
+
+        public static CategoryFieldListRes fromEntityList(List<CategoryFieldDefinitions> entities) {
+            List<CategoryFieldDetailRes> list = entities.stream()
+                    .map(CategoryFieldDetailRes::fromEntity)
+                    .collect(Collectors.toList());
+            return CategoryFieldListRes.builder()
+                    .categoryFields(list)
+                    .build();
+        }
     }
 
 }
