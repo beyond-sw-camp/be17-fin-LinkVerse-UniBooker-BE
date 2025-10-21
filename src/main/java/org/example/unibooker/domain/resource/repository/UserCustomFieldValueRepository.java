636d5b83e2628458d2fe45a9d36e971e8a9fc621
thay123028 <thay123028@naver.com>
2025-10-22 01:40:31 +0900

    [Feat] 특정 리소스의 커스텀 필드 값 찾는 코드 추가
    삭제된 커스텀 필드 값은 제외했습니다.

--- src/main/java/org/example/unibooker/domain/resource/repository/UserCustomFieldValueRepository.java
+++ src/main/java/org/example/unibooker/domain/resource/repository/UserCustomFieldValueRepository.java
@@ -4,6 +4,9 @@ import org.example.unibooker.domain.resource.model.UserCustomFieldValues;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface UserCustomFieldValueRepository extends JpaRepository<UserCustomFieldValues, Long> {
+    List<UserCustomFieldValues> findByReservationIdAndDeletedAtIsNull(Long reservationId);
 }
