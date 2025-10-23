9df47dea92ff653eeb882b899953f0bd52f450b6
yhg <hghgy@hansung.ac.kr>
2025-10-23 15:41:42 +0900

    [Modify] 예약 목록 조회 로직 수정

--- src/main/java/org/example/unibooker/domain/reservation/service/ReservationService.java
+++ src/main/java/org/example/unibooker/domain/reservation/service/ReservationService.java
@@ -8,10 +8,7 @@ import org.example.unibooker.domain.reservation.model.dto.ReservationDto;
 import org.example.unibooker.domain.reservation.model.entity.ReservationStatus;
 import org.example.unibooker.domain.reservation.model.entity.Reservations;
 import org.example.unibooker.domain.reservation.repository.ReservationRepository;
-import org.example.unibooker.domain.resource.model.CustomFieldDto;
-import org.example.unibooker.domain.resource.model.ResourceStatus;
-import org.example.unibooker.domain.resource.model.Resources;
-import org.example.unibooker.domain.resource.model.UserCustomFieldValues;
+import org.example.unibooker.domain.resource.model.*;
 import org.example.unibooker.domain.resource.repository.ResourceGroupRepository;
 import org.example.unibooker.domain.resource.repository.ResourceRepository;
 import org.example.unibooker.domain.resource.service.CustomFieldValueService;
@@ -56,37 +53,27 @@ public class ReservationService {
 
 
     /**
-     * 예약 목록 조회 - 플랫폼 관리자 및 기업 관리자 "리소스 그룹"의 목록
+     * 예약 목록 조회 - 플랫폼 관리자 및 기업 관리자
+     * 리소스 그룹에 예약된 예약/신청된 목록
      * */
-    public ReservationDto.ResponseList getAdminResourceGroupReservations(Long resourceGroupId) {
+    public ReservationDto.ResponseList getAdminReservations(Long resourceGroupId) {
         // 리소스 그룹 존재 여부 체크
-        resourceGroupRepository.findById(resourceGroupId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESOURCE_GROUP_NOT_FOUND));
+        ResourceGroups resourceGroups = resourceGroupRepository.findByIdAndDeletedAtIsNull(resourceGroupId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESOURCE_GROUP_NOT_FOUND));
 
         // 리소스 그룹 별 전체 예약 목록 조회
+        // TODO : 취소된 예약은 안보이게 조회하는 코드로 수정
         List<Reservations> result = reservationRepository.findAllByResourceGroupIdWithReservation(resourceGroupId);
-        return ReservationDto.ResponseList.from(result);
-    }
 
-
-    /**
-     * 예약 목록 조회 - 플랫폼 관리자 및 기업 관리자 "리소스"의 목록
-     */
-    public ReservationDto.ResponseList getAdminResourceReservations(Long resourceId) {
-        // 리소스 존재 여부 체크
-        resourceRepository.findById(resourceId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESOURCE_NOT_FOUND));
-
-        // 리소스 별 전체 예약 목록 조회
-        List<Reservations> result = reservationRepository.findAllByResourceIdWithReservation(resourceId);
-        return ReservationDto.ResponseList.from(result);
+        return ReservationDto.ResponseList.from(result, resourceGroups.getCategory());
     }
 
 
     /**
      * 예약 목록 조회 - 일반 사용자
      * */
-    public ReservationDto.ResponseList getUserReservations(Long userId) {
+    public ReservationDto.UserResponseList getUserReservations(Long userId) {
         List<Reservations> result = reservationRepository.findAllByUsersId(userId);
-        return ReservationDto.ResponseList.from(result);
+        return ReservationDto.UserResponseList.from(result);
     }
 
 
@@ -94,6 +81,7 @@ public class ReservationService {
      * 예약 상세 조회
      */
     public ReservationDto.Response getReservationDetail(Long reservationId) {
+        // 예약 존재 여부 체크
         Reservations reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESERVATION_NOT_FOUND));
 
         // 하나의 예약에 대한 사용자 압력 커스텀 필드 값 리스트
@@ -113,11 +101,11 @@ public class ReservationService {
      * 예약 취소
      */
     public void cancel(Long reservationId) {
-        //예약 내역 존재 여부 체크
-        Reservations reservation = reservationRepository.findById(reservationId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESERVATION_NOT_FOUND));
+        // 예약 내역 존재 여부 체크
+        Reservations reservation = reservationRepository.findByIdAndDeletedAtIsNull(reservationId).orElseThrow(() -> new BaseException(BaseResponseStatus.RESERVATION_NOT_FOUND));
 
         // 예약이 취소된 적 있는지 체크
-        if(reservation.getDeletedAt() != null && reservation.getStatus() != ReservationStatus.CANCELLED) {
+        if(reservation.getStatus() != ReservationStatus.CANCELLED) {
             throw new BaseException(BaseResponseStatus.RESERVATION_ALREADY_CANCELED);
         }
 
