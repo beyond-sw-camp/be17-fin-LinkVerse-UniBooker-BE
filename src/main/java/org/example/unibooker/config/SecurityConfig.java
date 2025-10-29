b0b532c0cff294fd2018add98e17dc8692a096dd
somminn <cowmin104@gmail.com>
2025-10-29 16:47:20 +0900

    [Fix] SecurityConfig 수정

--- src/main/java/org/example/unibooker/config/SecurityConfig.java
+++ src/main/java/org/example/unibooker/config/SecurityConfig.java
@@ -154,8 +154,7 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(Arrays.asList(
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
-                "https://unibooker.local",
-                "https://unibooker.local:31211"
+                "https://www.unibooker.kro.kr:4040"
         ));
 
         // 허용할 HTTP 메서드
