5b71db23532526e38e13820611042e3746a89981
somminn <cowmin104@gmail.com>
2025-10-30 15:05:45 +0900

    [Fix] Config 수정

--- src/main/java/org/example/unibooker/config/SecurityConfig.java
+++ src/main/java/org/example/unibooker/config/SecurityConfig.java
@@ -156,7 +156,7 @@ public class SecurityConfig {
                 "http://127.0.0.1:5173",
                 "https://www.unibooker.kro.kr:4040",
                 "https://www.unibooker.kro.kr:443",
-                "https://www.unibooker.kro.kr:80"
+                "http://www.unibooker.kro.kr:80"
         ));
 
         // 허용할 HTTP 메서드
