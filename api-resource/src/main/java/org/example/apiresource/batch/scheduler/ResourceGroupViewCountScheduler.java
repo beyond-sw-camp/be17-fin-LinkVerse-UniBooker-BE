3423c009934bab0597e7badd6d5069ce2fe2a82f
Seoyeon Hong <hsy2709@gmail.com>
2025-11-18 20:54:37 +0900

    [Fix] batch 정지(임시)

--- api-resource/src/main/java/org/example/apiresource/batch/scheduler/ResourceGroupViewCountScheduler.java
+++ api-resource/src/main/java/org/example/apiresource/batch/scheduler/ResourceGroupViewCountScheduler.java
@@ -6,6 +6,7 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,22 +15,27 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class ResourceGroupViewCountScheduler {
 
+    @Value("${app.batch.viewcount.enabled:false}")
+    private boolean enabled;
+
     private final JobLauncher jobLauncher;
     private final Job resourceGroupViewCountJob;
 
     // 매 시간 정각 실행
     @Scheduled(cron = "0 0 * * * *")
     public void runViewCountBackupJob() {
-        try {
-            JobParameters params = new JobParametersBuilder()
-                    .addLong("timestamp", System.currentTimeMillis()) // 중복 실행 방지
-                    .toJobParameters();
+        if (!enabled) {
+            try {
+                JobParameters params = new JobParametersBuilder()
+                        .addLong("timestamp", System.currentTimeMillis()) // 중복 실행 방지
+                        .toJobParameters();
 
-            jobLauncher.run(resourceGroupViewCountJob, params);
+                jobLauncher.run(resourceGroupViewCountJob, params);
 
-            log.info("[Batch] ResourceGroup viewCount 백업 성공");
-        } catch (Exception e) {
-            log.error("[Batch] ResourceGroup viewCount 백업 실패", e);
+                log.info("[Batch] ResourceGroup viewCount 백업 성공");
+            } catch (Exception e) {
+                log.error("[Batch] ResourceGroup viewCount 백업 실패", e);
+            }
         }
     }
 }
